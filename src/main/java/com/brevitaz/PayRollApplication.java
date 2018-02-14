3c2bfd87f63847190f6696168a3713641758ac99
yashpatel <yashpatel2236@gmail.com>
2018-02-14 18:55:02 +0530

    xyz

--- src/main/java/com/brevitaz/PayRollApplication.java
+++ src/main/java/com/brevitaz/PayRollApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
 @ComponentScan("com.brevitaz")
 public class PayRollApplication {
 
