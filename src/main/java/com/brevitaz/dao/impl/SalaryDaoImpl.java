92c5ec90e342f3cd0e5f90edfa00634b62d6a5b5
yashpatel <yashpatel2236@gmail.com>
2018-02-15 12:36:36 +0530

    module with SalaryController has pushed.

--- src/main/java/com/brevitaz/dao/impl/SalaryDaoImpl.java
+++ src/main/java/com/brevitaz/dao/impl/SalaryDaoImpl.java
@@ -1,6 +1,35 @@
 package com.brevitaz.dao.impl;
 
 import com.brevitaz.dao.SalaryDao;
+import com.brevitaz.model.Employee;
+import com.brevitaz.model.Salary;
+
+import java.io.IOException;
+import java.util.List;
 
 public class SalaryDaoImpl implements SalaryDao {
+    @Override
+    public boolean create(Salary salary) throws IOException {
+        return false;
+    }
+
+    @Override
+    public List<Salary> getAll() throws IOException {
+        return null;
+    }
+
+    @Override
+    public boolean update(Salary salary, String id) throws IOException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) throws IOException {
+        return false;
+    }
+
+    @Override
+    public Employee getById(String id) throws IOException {
+        return null;
+    }
 }
