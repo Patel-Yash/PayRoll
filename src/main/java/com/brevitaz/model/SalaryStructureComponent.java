5bfaf945c3e0ce6a513a4f56674d1ea528995c42
arpy <arpydoshi28@gmail.com>
2018-02-15 10:54:02 +0530

    SalaryStructureComponentDao committed..

--- src/main/java/com/brevitaz/model/SalaryStructureComponent.java
+++ src/main/java/com/brevitaz/model/SalaryStructureComponent.java
@@ -7,8 +7,17 @@ public class SalaryStructureComponent {
 
     private String id;
     private String name;
-    private enum type {percentage,fixValue;}
+    private Type type;
+    private enum Type {percentage,fixValue;}
     private double value;
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
 
     public String getId() {
         return id;
