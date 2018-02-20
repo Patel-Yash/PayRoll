4b33d7896535ff0f784312a3f8444334c444e434
yashpatel <yashpatel2236@gmail.com>
2018-02-20 11:45:41 +0530

    salaryslip controller done.

--- src/main/java/com/brevitaz/controller/SalaryStructureComponentController.java
+++ src/main/java/com/brevitaz/controller/SalaryStructureComponentController.java
@@ -20,32 +20,26 @@ public class SalaryStructureComponentController {
     @RequestMapping(method = RequestMethod.POST)
     public boolean create(@RequestBody SalaryStructureComponent salaryStructureComponent) throws IOException {
         return salaryStructureComponentDao.create(salaryStructureComponent);
-
     }
 
 
     @RequestMapping(method = RequestMethod.GET)
     public List<SalaryStructureComponent> getAll() throws IOException {
         return salaryStructureComponentDao.getAll();
-
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public boolean update(@RequestBody SalaryStructureComponent salaryStructureComponent, @PathVariable String id) throws IOException {
         return salaryStructureComponentDao.update(salaryStructureComponent,id);
-
     }
 
     @RequestMapping(value = "/{id}", method = {RequestMethod.DELETE})
     public boolean delete(@PathVariable String id) throws IOException {
         return salaryStructureComponentDao.delete(id);
-
     }
 
     @RequestMapping(value = "/{id}", method = {RequestMethod.GET})
     public SalaryStructureComponent getById(@PathVariable String id) throws IOException {
         return salaryStructureComponentDao.getById(id);
-
     }
-
 }
