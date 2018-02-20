3222ee6dbe6a5dc7663f7d85e091f62ab5c9df22
yashpatel <yashpatel2236@gmail.com>
2018-02-20 12:07:26 +0530

    salaryslip controller done.

--- src/main/java/com/brevitaz/controller/SalaryStructureController.java
+++ src/main/java/com/brevitaz/controller/SalaryStructureController.java
@@ -1,7 +1,9 @@
 package com.brevitaz.controller;
 
+import com.brevitaz.model.Employee;
 import com.brevitaz.model.SalaryStructureComponent;
 import com.brevitaz.model.SalaryStructure;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -11,20 +13,38 @@ import java.util.List;
 @RequestMapping("/salary-structure")
 public class SalaryStructureController {
 
+    @Autowired
+    SalaryStructureDao salaryStructureDao;
+
     @RequestMapping(method = RequestMethod.POST)
     public boolean create(@RequestBody SalaryStructure salaryStructure)
     {
-        return true;
+        return salaryStructureDao.create(salaryStructure);
     }
 
     @RequestMapping(method = RequestMethod.GET)
     public SalaryStructure getById(@PathVariable String id)
     {
-        return null;
+        return salaryStructureDao.getById(id);
     }
 
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public boolean update(@RequestBody SalaryStructure salaryStructure, @PathVariable String id) throws IOException {
+        return salaryStructure.update(salaryStructure,id);
 
+    }
 
+    @RequestMapping(value = "/{id}", method = {RequestMethod.DELETE})
+    public boolean delete(@PathVariable String id) throws IOException {
+        return salaryStructureDao.delete(id);
+
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<SalaryStructure> getAll() throws IOException {
+        return salaryStructureDao.getAll();
+
+    }
 
 
 
