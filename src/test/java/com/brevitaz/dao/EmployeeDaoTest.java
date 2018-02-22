3d9f2f7da1b6ec50d24244a87fdee297ede00f10
arpy <arpydoshi28@gmail.com>
2018-02-22 15:55:07 +0530

    Merge branch 'master' of https://github.com/Patel-Yash/PayRoll
    # Conflicts:
    #	src/main/java/com/brevitaz/model/SalarySlip.java

--- src/test/java/com/brevitaz/dao/EmployeeDaoTest.java
+++ src/test/java/com/brevitaz/dao/EmployeeDaoTest.java
@@ -24,8 +24,8 @@ public class EmployeeDaoTest
     public void createTest() throws IOException {
         Employee employee = new Employee();
         employee.setId("1");
-        employee.setName("Arpy");
-        //employee.setDate("2018-02-22");
+        employee.setName("Arpy");/*
+        employee.setDate(2018-02-22);*/
         employee.setDepartment("Developing");
         employee.setDesignation("Developer");
         employee.setLocation("Ahmedabad");
@@ -36,29 +36,71 @@ public class EmployeeDaoTest
 
     @Test
     public void getAllTest() throws IOException {
+        Employee employee = new Employee();
+        employee.setId("3");
+        employee.setName("dffbf");
+        employee.setDepartment("ndfhfjd");
+
+        boolean status = employeeDao.create(employee);
+        Assert.assertEquals(true,status);
+
         List<Employee> employees = employeeDao.getAll();
         int size = employees.size();
-        Assert.assertEquals(1,size);
+        Assert.assertEquals(4,size);
     }
 
     @Test
     public void getTest() throws IOException {
-        Employee employee = employeeDao.getById("1");
+        Employee employee = new Employee();
+        employee.setId("3");
+        employee.setName("Pr");/*
+        employee.setDate(2018-02-22);*/
+        employee.setDepartment("Developing");
+        employee.setDesignation("Developer");
+        employee.setLocation("Ahmedabad");
+
+        boolean status = employeeDao.create(employee);
+        Assert.assertEquals(true,status);
+
+        employee = employeeDao.getById("3");
         Assert.assertNotNull(employee);
     }
 
     @Test
     public void updateTest() throws IOException {
         Employee employee = new Employee();
-        employee.setName("Yash");
-        boolean status = employeeDao.update(employee,"1");
+        employee.setId("4");
+        employee.setName("Anushka");/*
+        employee.setDate(2018-02-22);*/
+        employee.setDepartment("Developing");
+        employee.setDesignation("Developer");
+        employee.setLocation("Ahmedabad");
+
+        boolean status = employeeDao.create(employee);
         Assert.assertEquals(true,status);
+
+        employee.setName("Anoushka");
+
+        boolean status1 = employeeDao.update(employee,"4");
+        Assert.assertEquals(true,status1);
     }
 
     @Test
     public void deleteTest() throws IOException {
-        boolean status=employeeDao.delete("1");
+
+        Employee employee = new Employee();
+        employee.setId("5");
+        employee.setName("Adhi");/*
+        employee.setDate(2018-02-22);*/
+        employee.setDepartment("Developing");
+        employee.setDesignation("Developer");
+        employee.setLocation("Ahmedabad");
+
+        boolean status = employeeDao.create(employee);
         Assert.assertEquals(true,status);
+
+        boolean status1=employeeDao.delete("5");
+        Assert.assertEquals(true,status1);
     }
 
 }
